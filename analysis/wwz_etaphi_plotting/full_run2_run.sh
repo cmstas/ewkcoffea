7e37e770d9e84c444b9ba93c9df47750b5ccc6d8
Matthew Dittrich <matthew.dittrich@ufl.edu>
2025-02-26 08:44:56 -0800

    Consolodating run scripts

--- analysis/wwz_etaphi_plotting/full_run2_run.sh
+++
@@ -1,4 +0,0 @@
-# Some example commands for running at scale
-
-# Run at scale with futures
-time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_4lskim_run2_v1.cfg -x futures -n 100 -o r2_wwz_histos_noSyst
--- analysis/wwz_etaphi_plotting/full_run3_run.sh
+++
@@ -1,7 +0,0 @@
-# Some example run commands for R3 configs
-# ALL RUN SCRIPTS ARE TO BE RUN LOCALLY AT UAF!
-
-#################################################################################################################################
-
-# 2022 + 2023
-time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_4lskim_2022_v1.cfg,../../input_samples/cfgs/wwz_analysis/samples_4lskim_2023_v1.cfg -o r3_wwz_histos_noSyst -x futures -n 100
