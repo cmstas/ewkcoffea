32eb14aa78d12209bfabafc567dad68715ba3057
Kelci Mohrman <kelci.mohrman@cern.ch>
2025-06-14 15:15:05 -0700

    Add run script

---
+++ analysis/vbs_vvh/run_wrapper.sh
@@ -0,0 +1,3 @@
+# Example run command
+#time python run_vvh.py input_cfg_r2.cfg -x futures -n 64 -o vvh_hists_njets --hist-list njets njets_counts
+time python run_vvh.py input_cfg_r2.cfg -x futures -n 64 -o vvh_hists
