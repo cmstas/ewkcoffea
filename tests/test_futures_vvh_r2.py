2bf571813fd402a8ba7a734ab39331f075f6df37
Kelci Mohrman <kelci.mohrman@cern.ch>
2025-07-29 14:09:14 -0700

    Update ref yields, fix ci

--- tests/test_futures_vvh_r2.py
+++ tests/test_futures_vvh_r2.py
@@ -11,8 +11,10 @@ def test_ewkcoffea_vvh():
         "input_samples/sample_jsons/test_samples/UL17_WWZJetsTo4L2Nu_forCI.json",
         "-o",
         "output_check_yields",
-        "-p",
+        "-outpath",
         "analysis/vbs_vvh/histos/"
+        "-p",
+        "1lep1jf_nano",
     ]
 
     # Run ewkcoffea
