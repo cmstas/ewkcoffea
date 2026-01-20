a4bb634e84ac61e2358d6c90e6024225cadab3da
Kelci Mohrman <kelci.mohrman@cern.ch>
2026-01-20 07:56:23 -0800

    Add the code for checking gen yields for BR study

--- analysis/vbs_vvh/analysis_processor_gen_fromnano.py
+++ analysis/vbs_vvh/analysis_processor_gen_fromnano.py
@@ -13,7 +13,7 @@ get_tc_param = GetParam(topcoffea_path("params/params.json"))
 
 class AnalysisProcessor(processor.ProcessorABC):
 
-    def __init__(self, samples, wc_names_lst=[], hist_lst=None, do_systematics=False, skip_obj_systematics=False, skip_signal_regions=False, skip_control_regions=False, muonSyst='nominal', dtype=np.float32, siphon_bdt_data=False):
+    def __init__(self, samples, wc_names_lst=[], hist_lst=None, do_systematics=False, skip_obj_systematics=False, skip_signal_regions=False, skip_control_regions=False, muonSyst='nominal', dtype=np.float32, siphon_bdt_data=False, rwgt_to_sm=None):
 
         self._samples = samples
         self._wc_names_lst = wc_names_lst
@@ -21,12 +21,9 @@ class AnalysisProcessor(processor.ProcessorABC):
 
         # Create the dense axes for the histograms
         self._dense_axes_dict = {
-            "l0_pt"   : axis.Regular(60, 0, 500, name="l0_pt", label="l0 pt"),
-            "j0_pt"   : axis.Regular(60, 0, 1000, name="j0_pt", label="j0 pt"),
-            "fj0_pt"  : axis.Regular(30, 0, 700, name="fj0_pt", label="fj0 pt"),
-            "nleps"   : axis.Regular(10, 0, 20, name="nleps",   label="Lep multiplicity"),
-            "njets"   : axis.Regular(10, 0, 20, name="njets",   label="Jet multiplicity"),
-            "nfj"     : axis.Regular(10, 0, 10, name="nfj",   label="Fat jet multiplicity"),
+            "l0_pt" : axis.Regular(60, 0, 500, name="l0_pt", label="l0 pt"),
+            "nlep"  : axis.Regular(10, 0, 20, name="nlep",   label="Lep multiplicity"),
+            "nnu"   : axis.Regular(10, 0, 20, name="nnu",   label="Nu multiplicity"),
 
         }
 
@@ -63,81 +60,76 @@ class AnalysisProcessor(processor.ProcessorABC):
         sow                = self._samples[json_name]["nSumOfWeights"]
         year               = self._samples[json_name]["year"]
 
-        # Initialize objects
-        met  = events.PuppiMET
-        #ele  = events.Electron
-        #mu   = events.Muon
-        #jets = events.Jet
-        fatjets = events.FatJet
+        #ele   = events.GenPart[(abs(events.GenPart.pdgId)==11) & events.GenPart.hasFlags("isLastCopy")]
+        #mu    = events.GenPart[(abs(events.GenPart.pdgId)==13) & events.GenPart.hasFlags("isLastCopy")]
+        #nuele = events.GenPart[(abs(events.GenPart.pdgId)==12) & events.GenPart.hasFlags("isLastCopy")]
+        #numu  = events.GenPart[(abs(events.GenPart.pdgId)==14) & events.GenPart.hasFlags("isLastCopy")]
+        #ele   = events.GenPart[(abs(events.GenPart.pdgId)==11)]
+        #muo   = events.GenPart[(abs(events.GenPart.pdgId)==13)]
+        #nuele = events.GenPart[(abs(events.GenPart.pdgId)==12)]
+        #numuo = events.GenPart[(abs(events.GenPart.pdgId)==14)]
+        #ele   = ele[(ele.parent.pdgId==23)     | (ele.parent.pdgId==24)   | (ele.parent.pdgId==-24)]
+        #muo   = muo[(muo.parent.pdgId==23)     | (muo.parent.pdgId==24)   | (muo.parent.pdgId==-24)]
+        #nuele = nuele[(nuele.parent.pdgId==23) | (nuele.parent.pdgId==24) | (nuele.parent.pdgId==-24)]
+        #numuo = numuo[(numuo.parent.pdgId==23) | (numuo.parent.pdgId==24) | (numuo.parent.pdgId==-24)]
+        #leps = ak.with_name(ak.concatenate([ele,muo],axis=1),'PtEtaPhiMCandidate')
+        #leps = leps[ak.argsort(leps.pt, axis=-1,ascending=False)] # Sort by pt
+        #neutrinos = ak.with_name(ak.concatenate([nuele,numuo],axis=1),'PtEtaPhiMCandidate')
 
-        ele = events.GenPart[abs(events.GenPart.pdgId)==11]
-        mu = events.GenPart[abs(events.GenPart.pdgId)==13]
-        jets = events.GenJet
+        lep = events.GenPart[(abs(events.GenPart.pdgId)==11) | (abs(events.GenPart.pdgId)==13) | (abs(events.GenPart.pdgId)==15)]
+        nu  = events.GenPart[(abs(events.GenPart.pdgId)==12) | (abs(events.GenPart.pdgId)==14) | (abs(events.GenPart.pdgId)==16)]
 
-        events.nom = ak.ones_like(met.pt)
+        lep_parent_is_v = ak.fill_none((lep.parent.pdgId==23) | (lep.parent.pdgId==24) | (lep.parent.pdgId==-24),False)
+        nu_parent_is_z  = ak.fill_none((nu.parent.pdgId==23),False)
+        lep = lep[lep_parent_is_v]
+        nu = nu[nu_parent_is_z]
 
 
         ################### Obj selection ####################
 
-        l_vvh_t = ak.with_name(ak.concatenate([ele,mu],axis=1),'PtEtaPhiMCandidate')
-        l_vvh_t = l_vvh_t[ak.argsort(l_vvh_t.pt, axis=-1,ascending=False)] # Sort by pt
-        events["l_vvh_t"] = l_vvh_t
+        lep_padded = ak.pad_none(lep, 4)
+        l0 = lep_padded[:,0]
+        nlep = ak.num(lep)
 
-        l_vvh_t_padded = ak.pad_none(l_vvh_t, 4)
-        l0 = l_vvh_t_padded[:,0]
-        nleps = ak.num(l_vvh_t)
+        nnu = ak.num(nu)
+        hasMet = nnu > 0
+        noMet = nnu == 0
 
-        # Count jets
-        goodJets = jets
-        njets = ak.num(goodJets)
-        ht = ak.sum(goodJets.pt,axis=-1)
-
-        goodJets_ptordered = goodJets[ak.argsort(goodJets.pt,axis=-1,ascending=False)]
-        goodJets_ptordered_padded = ak.pad_none(goodJets_ptordered, 4)
-        j0 = goodJets_ptordered_padded[:,0]
-
-        nfj = ak.num(fatjets)
-        fj_ptordered = fatjets[ak.argsort(fatjets.pt,axis=-1,ascending=False)]
-        fj_ptordered_padded = ak.pad_none(fj_ptordered, 4)
-        fj0 = fj_ptordered_padded[:,0]
-
-
-        ######### Masks we need for the selection ##########
         # Put the variables we'll plot into a dictionary for easy access later
         dense_variables_dict = {
-            "l0_pt" : ak.where(nleps>0,l0.pt,0),
-            "j0_pt" : ak.where(njets>0,j0.pt,0),
-            "fj0_pt" : ak.where(nfj>0,fj0.pt,0),
-            "nleps" : nleps,
-            "njets" : njets,
-            "nfj" : nfj,
+            "l0_pt" : ak.where(nlep>0,l0.pt,0),
+            "nlep" : nlep,
+            "nnu" : nnu,
         }
 
 
         ######### Fill histos #########
 
-        #mask = njets > -1
         mask_dict = {
-            "all_events" : njets > -1,
-            "nlep1p" : nleps >= 1,
-            "nfj2p" : nfj >= 2,
-            "nlep1p_nfj2p" : (nleps >= 1) & (nfj>=2),
+            "all_events" : nlep > -1,
+            "0l_yMet" : ((nlep==0) & hasMet),
+            "0l_nMet" : ((nlep==0) & noMet) ,
+            "1l_yMet" : ((nlep==1) & hasMet),
+            "1l_nMet" : ((nlep==1) & noMet) ,#
+            "2l_yMet" : ((nlep==2) & hasMet),
+            "2l_nMet" : ((nlep==2) & noMet) ,
+            "3l_yMet" : ((nlep==3) & hasMet),
+            "3l_nMet" : ((nlep==3) & noMet) ,#
+            "4l_yMet" : ((nlep==4) & hasMet),
+            "4l_nMet" : ((nlep==4) & noMet) ,
+            "5l_yMet" : ((nlep==5) & hasMet),
+            "5l_nMet" : ((nlep==5) & noMet) ,#
+            "6l_yMet" : ((nlep==6) & hasMet),
+            "6l_nMet" : ((nlep==6) & noMet) ,#
+            #"7lp" : (nlep>=7),
         }
-        #weight = events.nom
-        #if ak.any(events["LHEReweightingWeight"]):
-        #    genw = events["LHEReweightingWeight"][:,60]
-        #else:
-        #    genw = events["genWeight"]
         genw = events["genWeight"]
-        #genw = events["LHEReweightingWeight"][:,60]
-        #genw = events["LHEReweightingWeight"][:,15]
-
         #lumi = 1000.0*get_tc_param(f"lumi_{year}")
         lumi = 1000.0*138
         weight = (xsec/sow)*genw*lumi
 
+        # Loop over variables and fill hists
         for dense_axis_name, dense_axis_vals in dense_variables_dict.items():
-
             for mask_name,mask in mask_dict.items():
 
                 # Fill the histos
